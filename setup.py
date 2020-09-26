e8e9dba9ba2119c0dda55892755cdd68786e4a44
Kotaro Saito <31097246+skotaro@users.noreply.github.com>
2020-09-27 00:40:28 +0900

    Modify setup.py to exclude sample_data

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(
     long_description = long_description,
     long_description_content_type = 'text/markdown',
     url = 'https://github.com/skotaro/pybeads',
-    packages = find_packages(),
+    packages = find_packages(exclude=['sample_data']),
     classifiers = [
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3.6',
